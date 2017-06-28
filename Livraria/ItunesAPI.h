bdde169e0c1e4be0f41bbcf494ad97203126ea89
XCODE <xcode@ENGs-iMac.local>
2017-06-28 09:15:59 -0300

    classe iTunes

---
+++ Livraria/ItunesAPI.h
@@ -0,0 +1,13 @@
+//
+//  ItunesAPI.h
+//  Livraria
+//
+//  Created by XCODE on 28/06/17.
+//  Copyright © 2017 XCODE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItunesAPI : NSObject
+
+@end
---
+++ Livraria/ItunesAPI.m
@@ -0,0 +1,13 @@
+//
+//  ItunesAPI.m
+//  Livraria
+//
+//  Created by XCODE on 28/06/17.
+//  Copyright © 2017 XCODE. All rights reserved.
+//
+
+#import "ItunesAPI.h"
+
+@implementation ItunesAPI
+
+@end
